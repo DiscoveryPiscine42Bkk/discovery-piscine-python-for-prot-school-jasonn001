7cd5ed3b355bafca0e8a18e36d854fa517fa7d3c
jasonn001 <40679@prot.ac.th>
2025-03-10 15:22:27 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+first_name = 'Tanatep'
+last_name = 'Choopueng'
+Whole_name = (first_name + ' ' + last_name)
+
+print("Hey, what's your first name? ", first_name)
+print("what's your last name? ", last_name)
+print("Full name: ", Whole_name)
